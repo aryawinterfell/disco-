140cf8a5e6cfb349811227d71ef88525ae3d1fdc
adriana <adrianamolello@gmail.com>
2016-01-30 03:51:59 +0000

    Inital Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Disco::Application.config.secret_key_base = '718bfcf35dbc010bfa640c0bd88520b7620b8f7d9fb599158ae6a18a2326e67178661f6bb3336a2a7b8d145ee59d974f83f3f818052287132f4e7c6506b5a8ab'
